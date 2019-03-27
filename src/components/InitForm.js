1282b0bfa95ecbdc3077e6dab37080aac7d8e245
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2019-03-27 15:30:07 -0300

    Estilização Elementos

--- src/components/InitForm.js
+++ src/components/InitForm.js
@@ -4,19 +4,21 @@ import { Field, reduxForm } from 'redux-form'
 //Componente
 const InitForm = (props) => {
     return(
-        <div>
-            <form action="">
-                <div>
-                    <label htmlFor=""></label>
-                    <Field
-                        name="name"
-                        placeholder="Enter your name"
-                        type="text"
-                        component="input"
-                    />
-                </div>
-            </form>
-        </div>
+        <form>
+            <div className="form-group">
+                <label htmlFor="name">Informe seu Nome:</label>
+                <Field
+                    name="name"
+                    placeholder="Enter your name"
+                    type="text"
+                    component="input"
+                    className="form-control"
+                />
+            </div>
+            <div className="form-group">
+                <button className="btn btn-primary">Enviar</button>
+            </div>
+        </form>
     )
 }
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,12 +5,20 @@ import InitForm from "../components/InitForm";
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="col">
           <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p>
-            <InitForm/>
+              <div className="card">
+                  <div className="card-header">Formulário</div>
+                  <div className="card-body">
+                      <div className="card-title">
+                            Titulo
+                      </div>
+                      <InitForm/>
+                  </div>
+                  <div className="card-footer">
+                      <p>Rodapé</p>
+                  </div>
+              </div>
           </p>
       </div>
     );
