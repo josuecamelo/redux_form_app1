b7be310992318cf2acbb2c812cf16c8c6bf3c7e7
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2019-03-28 20:06:30 -0300

    Validações -  Criado função de validação

--- src/components/InitForm.js
+++ src/components/InitForm.js
@@ -3,12 +3,43 @@ import {Field, reduxForm} from 'redux-form'
 import {connect} from "react-redux";
 import {getInitialFetch} from './../actions';
 
+const renderField = ({
+                         input,
+                         label,
+                         type,
+                         meta: {touched, error, warning}
+                     }) => (
+    <div className="form-group">
+        <label htmlFor="">{label}</label>
+        <input {...input} placeholder={label} type={type} className="form-control"/>
+        {touched &&
+        ((error && <span className="text-danger">{error}</span>) || (warning &&
+            <span className="text-warning">{warning}</span>))}
+
+    </div>
+)
+
+const validate = (values) => {
+    const errors = {};
+    const {name} = values;
+
+    if (!name) {
+        errors.name = 'Required';
+    }
+
+    if (name && name.toString().length <= 2) {
+        errors.name = 'To Short';
+    }
+
+    return errors;
+}
+
 class InitForm extends Component {
     componentWillMount() {
         this.props.fetchInitial();
     }
 
-    render(){
+    render() {
         const {onClick, onChangeName} = this.props;
         return (
             <form>
@@ -18,7 +49,7 @@ class InitForm extends Component {
                         name="name"
                         placeholder="Enter your name"
                         type="text"
-                        component="input"
+                        component={renderField}
                         className="form-control"
                         onChange={onChangeName}
                     />
@@ -32,19 +63,21 @@ class InitForm extends Component {
 }
 
 const mapStateToProps = (state) => {
-    return{
+    return {
         initialValues: state.name.data
     }
 }
 
-const mapDispatchToProps = (dispatch)=> {
+const mapDispatchToProps = (dispatch) => {
     return {
-        fetchInitial : () => dispatch(getInitialFetch())
+        fetchInitial: () => dispatch(getInitialFetch())
     }
 }
 
 export default connect(
     mapStateToProps, mapDispatchToProps
 )(reduxForm({
-    form: 'InitForm'
+    form: 'InitForm',
+    enableReinitialize: true,
+    validate
 })(InitForm));
